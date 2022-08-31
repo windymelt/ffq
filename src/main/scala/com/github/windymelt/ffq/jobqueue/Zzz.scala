3dd80f299c89ebb02182733d565a31bc1f8674e5
Windymelt <windymelt@3qe.us>
2022-09-01 01:22:35 +0900

    fixed zzz

--- src/main/scala/com/github/windymelt/ffq/jobqueue/Zzz.scala
+++ src/main/scala/com/github/windymelt/ffq/jobqueue/Zzz.scala
@@ -18,15 +18,25 @@ object Zzz {
     } yield new Zzz {
       def sleep: IO[Unit] =
         for {
-          _ <- refDef.get.map(
-            _.get
-          ) // caveat: Due to automatic context shift, you should write in single flatmap
+          _ <- IO.println("[zzz]sleeping")
+          d <- renew
+          _ <- d.get
         } yield ()
+
+      val renew = for {
+        d <- refDef.get
+        tg <- d.tryGet
+        d2 <- tg match {
+          case None    => IO.pure(d) // not completed
+          case Some(_) => Deferred[IO, Unit] // renew
+        }
+        d3 <- refDef.set(d2)
+      } yield d2
 
       def wakeUp: IO[Unit] =
         for {
+          _ <- IO.println("[zzz] waking up")
           d <- Deferred[IO, Unit].flatMap(refDef.getAndSet)
-          _ <- IO.print(".")
           _ <- d.complete(())
         } yield ()
     }
